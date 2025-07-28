5c646a5a3ebca5690145a21ba9f4ae72e707e48b
Zulfiqar Ali <11179904+zulfiqarpk92@users.noreply.github.com>
2025-07-28 18:08:03 +0500

    Update ProjectCreate.jsx

--- resources/js/Pages/ProjectCreate.jsx
+++ resources/js/Pages/ProjectCreate.jsx
@@ -17,7 +17,7 @@ export default function ProjectCreate({ auth, clients = [] }) {
         <AuthenticatedLayout user={auth.user} header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Add Project</h2>}>
             <Head title="Add Project" />
             <div className="py-12">
-                <div className="max-w-xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-2xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             <h1 className="text-2xl font-bold mb-4">Add Project</h1>
