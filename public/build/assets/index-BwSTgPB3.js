91649a9bc3704138c78ea4aadca4a993b46d18a5
jawadislam92 <jawadislam92@gmail.com>
2025-08-01 01:05:46 +0500

    Fixed pervious bugs

--- public/build/assets/index-BwSTgPB3.js
+++
@@ -1,1 +0,0 @@
-import{g as r,b as t}from"./app-BL1zW9h8.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ public/build/assets/index-Bxs9jJ1k.js
@@ -0,0 +1,1 @@
+import{g as r,b as t}from"./app-DYLSN_Zi.js";var a=t();const e=r(a);export{e as R,a as r};
